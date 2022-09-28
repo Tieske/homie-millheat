bda82a418248e86b0dd7358f5e10b93a5957980c
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-09-28 23:00:49 +0200

    update docker base image and lualogging version

--- bin/homiemillheat.lua
+++ bin/homiemillheat.lua
@@ -3,6 +3,9 @@
 --- Main CLI application.
 -- Reads configuration from environment variables and starts the Millheat-to-Homie bridge.
 -- Does not support any CLI parameters.
+--
+-- For configureing the log, use LuaLogging enviornment variable prefix `"HOMIE_LOG_"`, see
+-- "logLevel" in the example below.
 -- @module homiemillheat
 -- @usage
 -- # configure parameters as environment variables
@@ -12,62 +15,29 @@
 -- export MILLHEAT_PASSWORD="xxxxxxxx"
 -- export MILLHEAT_POLL_INTERVAL=5            # default: 15 seconds
 -- export HOMIE_MQTT_URI="mqtt://synology"    # format: "mqtt(s)://user:pass@hostname:port"
--- export HOMIE_LOG_LEVEL="info"              # default: "INFO"
 -- export HOMIE_DOMAIN="homie"                # default: "homie"
 -- export HOMIE_DEVICE_ID="millheat"          # default: "millheat"
 -- export HOMIE_DEVICE_NAME="M2H bridge"      # default: "Millheat-to-Homie bridge"
+-- export HOMIE_LOG_LOGLEVEL="info"           # default: "INFO"
 --
 -- # start the application
 -- homiemillheat
 
-local ansicolors = require "ansicolors" -- https://github.com/kikito/ansicolors.lua
 local ll = require "logging"
-
-local log_level = tostring(os.getenv("HOMIE_LOG_LEVEL") or "INFO"):upper()
-assert(type(ll[log_level]) == "string", "environment variable HOMIE_LOG_LEVEL invalid; '"..log_level.."' is not a valid log-level")
-
-local logger do -- configure the default logger
-  require "logging.console"
-
-  logger = ll.defaultLogger(ll.console {
-    logLevel = ll[log_level],
-    destination = "stderr",
-    timestampPattern = "%y-%m-%d %H:%M:%S",
-    logPatterns = {
-      [ll.DEBUG] = ansicolors("%date%{cyan} %level %message %{reset}(%source)\n"),
-      [ll.INFO] = ansicolors("%date %level %message\n"),
-      [ll.WARN] = ansicolors("%date%{yellow} %level %message\n"),
-      [ll.ERROR] = ansicolors("%date%{red bright} %level %message %{reset}(%source)\n"),
-      [ll.FATAL] = ansicolors("%date%{magenta bright} %level %message %{reset}(%source)\n"),
-    }
-  })
-end
-
-
 local copas = require "copas"
+require("logging.rsyslog").copas() -- ensure copas, if rsyslog is used
+local logger = assert(require("logging.envconfig").set_default_logger("HOMIE_LOG"))
+
 
 do -- set Copas errorhandler
   local lines = require("pl.stringx").lines
+
   copas.setErrorHandler(function(msg, co, skt)
-    -- TODO: remove this code once Copas 4.1.0 is released
-    local co_str = co == nil and "nil" or copas.getthreadname(co)
-    local skt_str = skt == nil and "nil" or copas.getsocketname(skt)
-
-    msg = ("%s (coroutine: %s, socket: %s)"):format(tostring(msg), co_str, skt_str)
-
-    if type(co) == "thread" then
-      -- regular Copas coroutine
-      msg = debug.traceback(co, msg)
-    else
-      -- not a coroutine, but the main thread, this happens if a timeout callback
-      -- (see `copas.timeout` causes an error (those callbacks run on the main thread).
-      msg = debug.traceback(msg, 2)
-    end
-
+    msg = copas.gettraceback(msg, co, skt)
     for line in lines(msg) do
       ll.defaultLogger():error(line)
     end
-  end , true)
+  end, true)
 end
 
 
@@ -92,7 +62,6 @@ logger:info("MILLHEAT_SECRET_TOKEN: ********")
 logger:info("MILLHEAT_USERNAME: %s", opts.millheat_username)
 logger:info("MILLHEAT_PASSWORD: ********")
 logger:info("MILLHEAT_POLL_INTERVAL: %d seconds", opts.millheat_poll_interval)
-logger:info("HOMIE_LOG_LEVEL: %s", log_level)
 logger:info("HOMIE_DOMAIN: %s", opts.homie_domain)
 logger:info("HOMIE_MQTT_URI: %s", opts.homie_mqtt_uri)
 logger:info("HOMIE_DEVICE_ID: %s", opts.homie_device_id)
--- homie-millheat-scm-1.rockspec
+++ homie-millheat-scm-1.rockspec
@@ -28,7 +28,6 @@ dependencies = {
   "lua >= 5.1, < 5.5",
   "homie",
   "lualogging >= 1.6.0, < 2",
-  "ansicolors",
   "millheat",
 }
 
