4cf653b67428cb9ba41decf17ce8e637dc8ce0fb
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-03-11 09:04:26 +0100

    chore(docker): cleanup dockerfile

--- Docker.sh
+++ Docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build --no-cache --progress plain --tag tieske/homie-millheat:dev .
-#docker image push tieske/homie-millheat:dev
+docker image push tieske/homie-millheat:dev
